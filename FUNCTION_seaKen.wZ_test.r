9bfef10351b5e9aa0074d64f1c8a6ab95c213cbb
BROWN Daniel <Daniel.T.BROWN@deq.oregon.gov>
2026-04-22 15:41:23 -0700

    A few updates to continue testing of new code.

--- FUNCTION_seaKen.wZ_test.r
+++ FUNCTION_seaKen.wZ_test.r
@@ -1,34 +1,33 @@
-seaKen.wZ_test <- function (x, params,
-                            #season_months = list(
-                            #  summer = 6:9,
-                            #  other = c(10:12, 1:5)
-                            )#)
-{
+seaKen.wZ_test <- function (chem, params) {
+
+params <- c("temp", "d_o", "do_sat", "bod", "ph", "nh3", "no2", "p", "ecoli") 
+
 ### Prep Data
-  x2 <- x |>
+  chem2 <- chem |>
   mutate(
-    Date = as.Date(Date),
+    Date = as.Date(Date, format = "%m/%d/%Y"),
     month = month(Date),
     season = if_else(month %in% 6:9, "summer", "other"), # Define seasons
     wy = if_else(month >= 10, year(Date) + 1, year(Date)) # Water year (Oct–Sep)
   )
 
 ### Aggregate to season
-x_seasonal <- x2 %>%
+chem_seasonal <- chem2 %>%
   group_by(Station, wy, season) %>%
-  summarise(
-    across(all_of(params), ~ mean(.x, na.rm = TRUE)),
+  dplyr::summarise(
+    across(all_of(params), 
+           ~ mean(.x, na.rm = TRUE)),
     .groups = "drop") |>
   complete(Station, wy, season = c("other", "summer")) |>
   arrange(Station, wy, season) |>
   mutate(season = factor(season, levels = c("other", "summer")))
 
 ### Run seaKen per station
-results <- x_seasonal %>%
+results <- chem_seasonal %>%
   group_by(Station) %>%
   group_modify(~{
-    
-    # Build matrix of parameters
+ 
+  # Build matrix of parameters
     mat <- as.matrix(.x[params])
     
     ts_mat <- ts(
@@ -41,11 +40,17 @@ results <- x_seasonal %>%
     
     tibble(
       parameter = params,
-      Z = sk$Z,
-      slope = sk$sen.slope,
-      slope_pct = sk$sen.slope.pct,
-      p_value = sk$p.value
-    )
+      slope = sk[,  "sen.slope"],
+      slope_pct = sk[, "sen.slope.pct"],
+      p_value = sk[, "p.value"]
+    ) |>
+      mutate(
+        trend = case_when(
+          p_value <= 0.1 & slope > 0 ~ "increasing",
+          p_value <= 0.1 & slope < 0 ~ "decreasing",
+          TRUE ~ "no trend"
+        )
+      )
   })
 
 return(results)
